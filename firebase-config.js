965e3acde6cc31f558c5eef9753b4c21af9d3de1
Alex Djeuneng Donkeng <alexdjeunang20@gmail.com>
2025-07-10 23:05:19 +0200

    🎉 Initial commit

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAqL4uqwvQgyhk7UpVeS6iWTp7iHJ_hOTU",
+  authDomain: "gte-party.firebaseapp.com",
+  projectId: "gte-party",
+  storageBucket: "gte-party.appspot.com",
+  messagingSenderId: "974822302289",
+  appId: "1:974822302289:web:990b0310b224be0f2f0e70"
+};
+
+// Initialiser Firebase (compat version)
+firebase.initializeApp(firebaseConfig);
